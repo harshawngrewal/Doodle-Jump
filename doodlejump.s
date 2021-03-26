ee7ca625b3c9205bbf53e05c1e17422903a3ee43
Harshawn Grewal <harshawngrewal010@gmail.com>
2021-03-25 23:01:03 -0400

    finished generate_character

--- doodlejump.s
+++ doodlejump.s
@@ -17,14 +17,13 @@
 	stepsArray:	.word  0x10008180, 0x10008820, 0x10008F40
 	
 	# this will hold the contents of the character in our game
-	personArray: 	.word 0x10008E40, 0x10008DC0 
+	personArray: 	.word 0x10008E40, 0x10008DC0, 0x10008DC4, 0x10008DC8, 0x10008D48, 0x10008DCC, 0x10008DD0, 0x10008E50
 			
 	
 .text
 	li $s1, 0xff0000	# $t1 stores the red colour code
 	li $s2, 0x00ff00	# $t2 stores the green colour code
 		
-
 	
 main_loop:
 	lw $t0, displayAddressStart # temp vars so that we can paint entire bitmap
@@ -34,7 +33,7 @@ main_loop:
 	
 	
 	add $t0, $zero, $zero # Will act as pointer to our array
-	addi $t1, $zero, 8 # will let us know the end pointer in our array
+	addi $t1, $zero, 32 # will let us know the end pointer in our array
 	la $t2, personArray
 	jal generate_character
 	
@@ -45,7 +44,6 @@ main_loop:
 	
 	
 	j Exit
-	
 		
 # need to intialize the background for the map 
 repaint:
